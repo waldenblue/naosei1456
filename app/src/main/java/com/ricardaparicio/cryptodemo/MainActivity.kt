8647d2c53c0b3eac3cc731f96664e9feafb859d9
waldenblue <108653705+waldenblue@users.noreply.github.com>
2020-12-01 11:40:33 +0000

    More logging

--- app/src/main/java/com/ricardaparicio/cryptodemo/MainActivity.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/MainActivity.kt
@@ -33,4 +33,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @ExperimentalMaterialApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    override fun onCreate(sav
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoDemoTheme {
+      
