66bdeaf1c7b83528b644f27cbf1dd8f68d9bce11
waldenblue <108653705+waldenblue@users.noreply.github.com>
2020-12-29 07:56:54 +0000

    Fix: Message for payment forms

--- app/src/main/java/com/ricardaparicio/cryptodemo/core/di/NetworkingModule.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/core/di/NetworkingModule.kt
@@ -1,2 +1,5 @@
 /*
- * Copyright 2022 Ricard A
+ * Copyright 2022 Ricard Aparicio
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the 
