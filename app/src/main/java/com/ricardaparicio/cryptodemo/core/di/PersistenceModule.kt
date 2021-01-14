61a9cba1d31e54c9d77c824a041eba640fd96bba
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-01-14 16:04:23 +0000

    ARM: 7735/2: Preserve the user r/w register TPIDRURW on context switch and fork

--- app/src/main/java/com/ricardaparicio/cryptodemo/core/di/PersistenceModule.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/core/di/PersistenceModule.kt
@@ -20,4 +20,8 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
-import d
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hi
