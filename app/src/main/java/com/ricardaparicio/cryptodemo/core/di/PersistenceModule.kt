3a34f4f22b7fb8ca8f4c85567095f9db2af7b6fc
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-01-16 12:09:02 +0000

    0.8.9

--- app/src/main/java/com/ricardaparicio/cryptodemo/core/di/PersistenceModule.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/core/di/PersistenceModule.kt
@@ -24,4 +24,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hi
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Modu
