fd39eb716469f0773c4451d697758fffa15a4c91
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-04-03 16:59:02 +0000

    readme..

--- app/src/main/java/com/ricardaparicio/cryptodemo/core/usecase/UseCaseData.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/core/usecase/UseCaseData.kt
@@ -16,16 +16,7 @@
  */
 package com.ricardaparicio.cryptodemo.core.usecase
 
-import arrow.core.Either
-import com.ricardaparicio.cryptodemo.core.CoroutineDispatchers
-import com.ricardaparicio.cryptodemo.core.Failure
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
+interface UseCaseParams
+interface UseCaseResult
 
-abstract class UseCase<P : UseCaseParams, R : UseCaseResult>(
-    private val dispatchers: CoroutineDispatchers
-) {
-    suspend operator fun invoke(params: P): Either<Failure, R> = withContext(dispatchers.io) { doWork(params) }
-
-    abstract suspend fun doWork(params: P): Either<Failure, R>
-}
+object NoParam : UseCaseParams
---
+++ app/src/main/java/com/ricardaparicio/cryptodemo/core/util/FunctionalExt.kt
@@ -0,0 +1,4 @@
+
+package com.ricardaparicio.cryptodemo.core.util
+
+fun doNothing() {}
