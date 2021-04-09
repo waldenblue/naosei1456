220cdc0cb994f4eaea36391e79617b942e0b0d4e
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-04-09 06:59:11 +0000

    Meteor update

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/coindetail/presentation/reducer/CoinDetailReducer.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/coindetail/presentation/reducer/CoinDetailReducer.kt
@@ -13,4 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ricardaparicio.cryptode
+package com.ricardaparicio.cryptodemo.features.coindetail.presentation.reducer
+
+import com.ricardaparicio.cryptodemo.core.Reducer
+import com.ricardaparicio.crypto
