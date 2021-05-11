da837ae936d051cb02966c5b3a34a3646fbb0f02
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-05-11 14:38:55 +0000

    changing directory structure

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/coindetail/presentation/ui/CoinDetailUi.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/coindetail/presentation/ui/CoinDetailUi.kt
@@ -19,4 +19,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.fou
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compo
