8f189a3c7c27ed5cc54a76e46041663a341f13a5
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-07-24 19:49:07 +0000

    6

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/coindetail/presentation/viewmodel/CoinDetailViewModel.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/coindetail/presentation/viewmodel/CoinDetailViewModel.kt
@@ -20,4 +20,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.li
+import androidx.lifecycle.viewModelScope
+import com.ricardaparicio.cryptodemo.core.Reducer
+import com.ricardaparicio.cryptodemo.c
