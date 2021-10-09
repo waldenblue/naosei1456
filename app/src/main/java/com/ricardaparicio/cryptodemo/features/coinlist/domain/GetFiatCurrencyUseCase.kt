4ec949e4fd3cdb70db10fa97b7da5880b28cb972
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-10-09 09:59:56 +0000

    Update vim bundles

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/coinlist/domain/GetFiatCurrencyUseCase.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/coinlist/domain/GetFiatCurrencyUseCase.kt
@@ -13,4 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ricard
+package com.ricardaparicio.cryptodemo.features.coinlist.domain
+
+import arrow.core.Either
+import com.ricardaparicio.cryptodemo.core.Coroutin
