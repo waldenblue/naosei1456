f27752eee55ac012b3e555ec2d21e84db70e0a8b
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-11-06 19:07:04 +0000

    Update plug.vim

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/coinlist/presentation/ui/CoinListUiState.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/coinlist/presentation/ui/CoinListUiState.kt
@@ -22,4 +22,5 @@ import com.ricardaparicio.cryptodemo.features.common.ui.model.CoinSummaryUiModel
 data class CoinListUiState(
     val coins: List<CoinSummaryUiModel> = emptyList(),
     val fiatCurrency: FiatCurrency = FiatCurrency.Eur,
-    val contentLoadingUiState: ContentLoadingUiS
+    val contentLoadingUiState: ContentLoadingUiState = ContentLoadingUiState.empty,
+)
---
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/coinlist/presentation/viewmodel/CoinListViewModel.kt
@@ -0,0 +1,81 @@
+
+/*
+ * Copyright 2022 Ricard Aparicio
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ricardaparicio.cryptodemo.features.coinlist.presentation.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import arrow.core.Either
+import com.ricardaparicio.cryptodemo.core.Failure
+import com.ricardaparicio.cryptodemo.core.Reducer
+import com.ricardaparicio.cryptodemo.core.usecase.NoParam
+import com.ricardaparicio.cryptodemo.features.coinlist.domain.GetCoinListUseCase
+import com.ricardaparicio.cryptodemo.features.coinlist.domain.GetFiatCurrencyUseCase
+import com.ricardaparicio.cryptodemo.features.coinlist.domain.UpdateFiatCurrencyUseCase
+import com.ricardaparicio.cryptodemo.features.coinlist.presentation.reducer.CoinListUiAction
+import com.ricardaparicio.cryptodemo.features.coinlist.presentation.reducer.CoinListUiAction.*
+import com.ricardaparicio.cryptodemo.features.coinlist.presentation.ui.CoinListUiState
+import com.ricardaparicio.cryptodemo.features.common.domain.model.CoinListState
+import com.ricardaparicio.cryptodemo.features.common.domain.model.FiatCurrency
+import com.ricardaparicio.cryptodemo.features.common.ui.viewmodel.ContentLoadingUiAction
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinListViewModel
+@Inject constructor(
+    private val getCoinListUseCase: GetCoinListUseCase,
+    private val getFiatCurrencyUseCase: GetFiatCurrencyUseCase,
+    private val updateFiatCurrencyUseCase: UpdateFiatCurrencyUseCase,
+    private val reducer: Reducer<CoinListUiState, CoinListUiAction>,
+) : ViewModel() {
+
+    var uiState by mutableStateOf(CoinListUiState())
+        private set
+
+    init {
+        viewModelScope.launch {
+            getFiatCurrency()
+            fetchCoins()
+        }
+    }
+
+    private suspend fun getFiatCurrency() =
+        getFiatCurrencyUseCase(NoParam)
+            .fold(
+                { failure ->
+                    reduce(UpdateContentLoading(ContentLoadingUiAction.Error(failure)))
+                },
+                { result ->
+                    reduce(UpdateFiatCurrency(result.currency))
+                }
+            )
+
+    private fun updateFiatCurrency(currency: FiatCurrency) =
+        viewModelScope.launch {
+            updateFiatCurrencyUseCase(UpdateFiatCurrencyUseCase.Params(currency))
+                .fold(
+                    { failure ->
+                        reduce(UpdateContentLoading(ContentLoadingUiAction.Error(failure)))
+                    },
+                    { result ->
+                        reduce(UpdateFiatCurrency(result.currency))
