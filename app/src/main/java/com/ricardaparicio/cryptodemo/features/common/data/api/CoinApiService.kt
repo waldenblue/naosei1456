a852ff20185ca9059b90a6028ac71c6a4c0cdc7e
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-11-20 16:08:53 +0000

    ignore more stuff

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/api/CoinApiService.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/api/CoinApiService.kt
@@ -13,4 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ricard
+package com.ricardaparicio.cryptodemo.features.common.data.api
+
+import com.ricardaparicio.cryptodemo.features.common.data.api.model.CoinAp
