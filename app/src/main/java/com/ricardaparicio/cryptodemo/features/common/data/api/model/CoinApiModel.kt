4d268d2e97709976406cc6e246ac8a36e18883b2
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-11-27 07:07:30 +0000

    Update matrix.md

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/api/model/CoinApiModel.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/api/model/CoinApiModel.kt
@@ -1,4 +1,8 @@
 package com.ricardaparicio.cryptodemo.features.common.data.api.model
 
 data class CoinApiModel(
- 
+    val id: String,
+    val symbol: String,
+    val name: String,
+    val links: CoinLinksApiModel,
+    val image: CoinIm
