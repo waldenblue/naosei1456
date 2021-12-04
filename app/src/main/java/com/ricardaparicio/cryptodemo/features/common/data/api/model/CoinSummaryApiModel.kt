1e7105f27345c4fe147793b219c5a5061033c73d
waldenblue <108653705+waldenblue@users.noreply.github.com>
2021-12-04 16:37:19 +0000

    found bug

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/api/model/CoinSummaryApiModel.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/api/model/CoinSummaryApiModel.kt
@@ -4,4 +4,7 @@ data class CoinSummaryApiModel(
     val id: String,
     val symbol: String,
     val name: String,
-    val image: String
+    val image: String,
+    val current_price: Float,
+    val market_cap_rank: Int,
+)
