8794e53ab4790e3612b8fb62b2c3afadb26a751b
waldenblue <108653705+waldenblue@users.noreply.github.com>
2022-03-13 17:12:49 +0000

    update year in license files

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/datasource/CoinRetrofitDataSource.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/datasource/CoinRetrofitDataSource.kt
@@ -13,4 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ricardaparicio.cryptodemo.features.c
+package com.ricardaparicio.cryptodemo.features.common.data.datasource
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.ricardaparicio.cryp
