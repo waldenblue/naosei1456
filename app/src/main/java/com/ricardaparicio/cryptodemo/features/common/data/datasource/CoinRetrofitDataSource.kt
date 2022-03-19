6e58d0d151739595ea2fe6020776d456f1dcd803
waldenblue <108653705+waldenblue@users.noreply.github.com>
2022-03-19 14:10:40 +0000

    Russian translation

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/datasource/CoinRetrofitDataSource.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/data/datasource/CoinRetrofitDataSource.kt
@@ -24,4 +24,7 @@ import com.ricardaparicio.cryptodemo.core.ServerError
 import com.ricardaparicio.cryptodemo.features.common.data.api.CoinApiService
 import com.ricardaparicio.cryptodemo.features.common.domain.model.Coin
 import com.ricardaparicio.cryptodemo.features.common.domain.model.CoinSummary
-import com.ricardapari
+import com.ricardaparicio.cryptodemo.features.common.domain.model.FiatCurrency
+import retrofit2.Call
+import timber.log.Timber
+import java.lang.I
