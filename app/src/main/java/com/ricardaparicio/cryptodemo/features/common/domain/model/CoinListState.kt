41f4bb8621a33e4ec4d7761349ba6f0ef8a6c56b
waldenblue <108653705+waldenblue@users.noreply.github.com>
2022-05-23 11:09:43 +0000

    Rename README.md to index.md

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/domain/model/CoinListState.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/domain/model/CoinListState.kt
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2022 Ricard Aparicio
 
@@ -15,11 +16,7 @@
  */
 package com.ricardaparicio.cryptodemo.features.common.domain.model
 
-data class Coin(
-    val coinSummary: CoinSummary,
-    val description: String,
-    val ath: Float,
-    val marketCap: Float,
-    val priceChange24h: Float,
-    val priceChangePercentage24h: Float,
-)
+sealed class CoinListState {
+    object Loading : CoinListState()
+    data class Coins(val coins: List<CoinSummary>): CoinListState()
+}
--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/domain/model/CoinSummary.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/domain/model/CoinSummary.kt
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2022 Ricard Aparicio
 
@@ -15,11 +16,12 @@
  */
 package com.ricardaparicio.cryptodemo.features.common.domain.model
 
-data class Coin(
-    val coinSummary: CoinSummary,
-    val description: String,
-    val ath: Float,
-    val marketCap: Float,
-    val priceChange24h: Float,
-    val priceChangePercentage24h: Float,
+data class CoinSummary(
+    val id: String,
+    val symbol: String,
+    val name: String,
+    val image: String,
+    val price: Float,
+    val marketCapRank: Int,
+    val fiatCurrency: FiatCurrency,
 )
