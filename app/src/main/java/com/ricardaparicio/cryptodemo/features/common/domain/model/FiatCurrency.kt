6c5949c1994b12fc27500d2b67f7a770ce71151f
waldenblue <108653705+waldenblue@users.noreply.github.com>
2022-06-16 12:08:30 +0000

    typo in previous commit

--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/domain/model/FiatCurrency.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/domain/model/FiatCurrency.kt
@@ -13,4 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.rica
+package com.ricardaparicio.cryptodemo.features.common.domain.model
+
+enum class FiatCurrency {
+    Eur, Usd
+}
