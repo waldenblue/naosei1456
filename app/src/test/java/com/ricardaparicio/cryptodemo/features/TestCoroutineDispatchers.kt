367c8346c035050ee5055c23f1cfa38ca083a78b
waldenblue <108653705+waldenblue@users.noreply.github.com>
2022-11-15 20:16:34 +0000

    Update matrix.cpp

--- app/src/test/java/com/ricardaparicio/cryptodemo/features/TestCoroutineDispatchers.kt
+++ app/src/test/java/com/ricardaparicio/cryptodemo/features/TestCoroutineDispatchers.kt
@@ -1,4 +1,6 @@
 /*
  * Copyright 2022 Ricard Aparicio
 
- * Licensed under the Apache
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obta
