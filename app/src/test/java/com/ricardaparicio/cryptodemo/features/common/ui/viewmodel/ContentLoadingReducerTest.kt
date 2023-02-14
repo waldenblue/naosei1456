327e1479042cdbbc85ed780905b711512e5c43e5
waldenblue <108653705+waldenblue@users.noreply.github.com>
2023-02-14 08:45:16 +0000

    More tweaks to README

--- app/src/test/java/com/ricardaparicio/cryptodemo/features/common/ui/viewmodel/ContentLoadingReducerTest.kt
+++ app/src/test/java/com/ricardaparicio/cryptodemo/features/common/ui/viewmodel/ContentLoadingReducerTest.kt
@@ -11,4 +11,8 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations u
+ * limitations under the License.
+ */
+package com.ricardaparicio.cryptodemo.features.common.ui.viewmodel
+
+import com.ricardaparic
